17ee6a97d10da48e36c1b09b423c944c7800a650
prakash s <55954247+prakashssp077@users.noreply.github.com>
2020-03-10 19:38:26 +0530

    Initial commit

---
+++ lib/StudentModel.dart
@@ -0,0 +1,15 @@
+class StudentModel {
+  final String name;
+  final String skill;
+  final String education;
+
+  StudentModel({this.name, this.skill, this.education});
+
+  factory StudentModel.fromjason(final json){
+    return StudentModel(
+        name: json["name"],
+        education: json["education"],
+        skill: json["skill"]
+    );
+  }
+}
